65c0243c01516b075428a91373bddd56bbf42a36
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-31 16:22:53 +0300

    Create a messages route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+// A promo message to the user
+const message = "Black Friday! Get 50% discount on saving your first spot.";
+router.get('/messages', function (req, res) {
+    res.end(JSON.stringify(message))
+});
+
 module.exports = router;
